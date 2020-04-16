89cec6c741cd8c1804991c2667a53c6a25d9e421
MeganSoto <62619472+MeganSoto@users.noreply.github.com>
2020-04-15 22:07:53 -0800

    los resultados

--- proceso.php
+++ proceso.php
@@ -2,24 +2,44 @@
     $valor1= $_POST['n1'];
     $valor2= $_POST['n2'];
     $op=$_POST['operacion'];
+	$resultado;
+	
 
     if($op==1)
 	{
-        echo "La suma es: ".($valor1+$valor2);
+  $resultado=($valor1+$valor2);
 	}
 
     if($op==2)
 	{
-        echo "La resta es: ".($valor1-$valor2);
+      $resultado=($valor1-$valor2);
 	}
 
     if($op==3)
 	{
-        echo "La multiplicacion es: ".($valor1*$valor2);
+       $resultado= ($valor1*$valor2);
 	}
 
     if($op==4)
 	{
-        echo "La division es: ".($valor1/$valor2);
+        $resultado=($valor1/$valor2);
 	}
-?>
+?> 
+
+  <html>
+    <head>
+	  <link rel="stylesheet" type="text/css" href="MAQUETACION.css" />
+    </head>
+	
+	<body>
+	<header>
+	<div align="center" id="RESULTADO">
+	<h1> El resultado es:<h1>
+	</div>
+	</header>
+ <section>
+ <div align="center" id="IGUAL">
+  <p><?php echo $resultado; ?> </p>
+  </section>
+	</body>
+	</html>
